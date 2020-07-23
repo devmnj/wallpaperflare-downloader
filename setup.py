106e6091b0554da35e07c21ecf4ae26cc8bacb02
Manoj AP <manojap.nair@gmail.com>
2020-07-23 17:37:12 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wallspyder',
+    version='1.0',
+    packages=['wallspyder'],
+    url='http://github.com/manojap/wallspyder',
+    license='GNU  OPEN SOURCE',
+    author='manoj',
+    author_email='MANOJAP@OUTLOOK.COM',
+    description='Collection Wallpaper downloader selenium browser auutomation'
+)
