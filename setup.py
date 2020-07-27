13d6af6ebbcdbcf08113fd8142063534c2cd250f
Manoj AP <manojap.nair@gmail.com>
2020-07-27 16:54:26 +0530

    setup changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wallspyder',
+    version='1.01',
+    packages=['wallspyder'],
+    url='',
+    license='Free',
+    author='Manoj A,P',
+    author_email='manojap@outlook.com',
+    description='Wallpaper Automation'
+)
