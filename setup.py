22e621add05b44103a9b1f3a229c369e42ddb6df
Manoj AP <manojap.nair@gmail.com>
2020-07-27 17:46:12 +0530

    setup changes

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import os
 from setuptools import setup
 
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(
-    name='wallspyder',
-    version='1.01',
+    name="wallspyder",
+    version="0.0.4",
+    author="Manoj A.P",
+    author_email="manojap@outlook.com",
+    description="Wallpaper downloading bots",
+    license="BSD",
+    keywords="wallpaper,download,stock images",
+    url="http://github.com/manojap/wallspyder",
     packages=['wallspyder'],
-    url='',
-    license='Free',
-    author='Manoj A,P',
-    author_email='manojap@outlook.com',
-    description='Wallpaper Automation'
+    long_description=read('readme.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Fun",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
