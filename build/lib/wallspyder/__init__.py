0fbba07dbbde6f15ca78f3030330bfd89412557b
Manoj AP <manojap.nair@gmail.com>
2020-07-28 20:15:39 +0530

    docs updates

---
+++ build/lib/wallspyder/__init__.py
@@ -0,0 +1,10 @@
+print('Thank you for trying wallspyder')
+
+"""
+A collection of selenium automation scripts for
+searching and downloading wallppaers, stock images
+for developing and designing.
+
+originallt wrritten by Manoj A.P 
+:print: http://github.com/manojap
+"""
--- build/lib/wallspyder/pexels.py
+++ build/lib/wallspyder/pexels.py
@@ -36,7 +36,7 @@ class search:
             if browser != 'Firefox':
                 if head_less is True:
                     self.chrome_opt.add_argument('--headless')
-                self.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(),chrome_options=self.chrome_opt)
+                self.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
             else:
                 self.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
